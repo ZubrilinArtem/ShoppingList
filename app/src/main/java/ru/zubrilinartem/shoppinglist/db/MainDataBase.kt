d532e8a398abd2ddc0c42b0c3d4aeb4723d9276a
artemzubrilin <zubrilinartem1994@gmail.com>
2022-01-30 15:36:10 +0300

    add MainDataBase.kt

---
+++ app/src/main/java/ru/zubrilinartem/shoppinglist/db/MainDataBase.kt
@@ -0,0 +1,28 @@
+package ru.zubrilinartem.shoppinglist.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import ru.zubrilinartem.shoppinglist.entities.LibraryItem
+import ru.zubrilinartem.shoppinglist.entities.NoteItem
+import ru.zubrilinartem.shoppinglist.entities.ShoppingListItem
+import ru.zubrilinartem.shoppinglist.entities.ShoppingListNames
+
+@Database(entities = [LibraryItem::class, NoteItem::class, ShoppingListItem::class, ShoppingListNames::class], version = 1)
+abstract class MainDataBase :RoomDatabase(){
+
+    companion object{
+        @Volatile
+        private var INSTANCE: MainDataBase? = null
+        fun getDataBase(context: Context): MainDataBase{
+            return INSTANCE ?: synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext
+                    , MainDataBase::class.java
+                    , "shopping_list.db").build()
+                instance
+            }
+        }
+    }
+}
